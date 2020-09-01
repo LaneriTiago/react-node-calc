253b803b387ad3f6536da58e21c392615ec9b3d2
Tiago Luis Laneri <tiago.laneri93@gmail.com>
2020-08-31 23:42:01 -0300

    first commit

---
+++ my-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './assets/global/style.css';
+import './assets/global/reset.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
---
+++ my-app/src/pages/home/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+
+function Home () {
+    return (
+        <div id="container">
+            <header id="header-style">
+                <h1 id="logo-header">CALCULANDO</h1>
+            </header>
+            <main id="main-style">
+                <section id="content-style">
+                </section>
+            </main>
+            <footer id="footer-style">
+                <p>Projeto desenvolvido em React e Node</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Home

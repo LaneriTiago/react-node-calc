053445827b5bcce7f4ac2cc3344c4b2e2796c378
Tiago Luis Laneri <tiago.laneri93@gmail.com>
2020-09-01 18:35:49 -0300

    css development

--- my-app/src/pages/home/index.js
+++ my-app/src/pages/home/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useFormik } from 'formik';
 import './style.css';
+import Calc from '../../assets/icon/calc-icon.svg';
+
 
 function Home () {
     return (
@@ -7,8 +10,33 @@ function Home () {
             <header id="header-style">
                 <h1 id="logo-header">CALCULANDO</h1>
             </header>
+            <div id="tutorial-style">
+                teste
+            </div>
             <main id="main-style">
                 <section id="content-style">
+                        <p id="content-header">
+                            <p><img src={Calc} id="content-icon-header" alt="ícone de cérebro"/>
+                            Vamos matar essa curiosidade 
+                            </p>
+                        </p>
+                        <form id="input-container">
+                            
+                          <div id="input-money">
+                            <label>Montante</label>
+                            <input type="text" id="input-style" placeholder="Digite aqui o valor recebido"/>
+                          </div>
+
+                          <div id="input-object">
+                            <label>Objeto ou serviço</label>
+                            <input type="text" id="input-style" placeholder="Digite aqui o valor do objeto ou serviço" />
+                          </div>
+
+                          <div id="submit-button">
+                            <button>Calcular</button>
+                          </div>
+                        </form>
+                        
                 </section>
             </main>
             <footer id="footer-style">
